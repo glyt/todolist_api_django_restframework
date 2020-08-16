6ef89e1ea305b2dd2864be29f47c1b8654cf9ee6
gilzgetuya <gilzgetuya@gmail.com>
2020-08-16 12:01:45 +0400

    First commit

---
+++ api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from todo.models import Todo
+
+
+# Retrieve/Update/Delete/Destroy
+class TodoSerializer(serializers.ModelSerializer):
+    created = serializers.ReadOnlyField()
+    datecompleted = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Todo
+        fields = ['id', 'title', 'memo', 'created', 'datecompleted', 'important']
+
+
+# Complete
+class TodoCompleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ['id']
+        read_only_fields = ['title', 'memo', 'created', 'datecompleted', 'important']
